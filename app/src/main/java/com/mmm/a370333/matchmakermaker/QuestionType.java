695996a67ea1a9947846f280a5b362cb564bf2e3
ShivLuthra <gbshiv12@yahoo.com>
2017-06-14 19:48:11 -0400

    June 14:  - Bundle Transfer  - Tracking of question left and fr

--- app/src/main/java/com/mmm/a370333/matchmakermaker/QuestionType.java
+++ app/src/main/java/com/mmm/a370333/matchmakermaker/QuestionType.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 /**
  * Created by 370333 on 5/30/17.
@@ -15,9 +16,19 @@ import android.widget.Spinner;
 public class QuestionType extends Activity{
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        int questionAmount = 0;
         final Bundle bundle = getIntent().getExtras();
-        int questionAmount = bundle.getInt("questionAmount");
+        if (getIntent() != null) {
+            questionAmount = (int) bundle.get("questionAmount");
+        }
+        /*if (bundle.get("fr_questions") != null){
+            System.out.println(((String[])bundle.get("fr_questions"))[(int)bundle.get("questionAmount")]);
+        }*/
+        bundle.putInt("questionAmount", questionAmount-1);
+        System.out.println(questionAmount);
+        questionAmount = (int) bundle.get("questionAmount");
+        final TextView questionNum = (TextView) findViewById(R.id.questionNumber);
+        //questionNum.setText("asdf" + questionAmount);
 
         if (questionAmount > 0) {
             setContentView(R.layout.questiontype);
@@ -26,15 +37,18 @@ public class QuestionType extends Activity{
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
             questionType.setAdapter(adapter);
 
-
+            questionAmount--;
             Button btn_next = (Button) findViewById(R.id.btn_make);
             btn_next.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     String type = questionType.getSelectedItem().toString();
                     if (type.equals("Free Response")) {
-
                         Intent int_next = new Intent(v.getContext(), MakeFreeResponse.class);
+                        int_next.putExtras(bundle);
+                        startActivityForResult(int_next, 0);
+                    } else if (type.equals("Multiple Choice")) {
+                        Intent int_next = new Intent(v.getContext(), MakeMultResponse.class);
                         int_next.putExtras(bundle);
                         startActivityForResult(int_next, 0);
                     }
