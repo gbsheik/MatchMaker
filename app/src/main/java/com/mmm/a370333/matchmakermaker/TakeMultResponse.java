53a5fc2285bf18638ee420065aef6e26cd759da2
gbsheik <shluthraiv@gmail.com>
2017-06-16 08:27:49 -0400

    SL 6/16-A:  - Take MultResponse Screen

---
+++ app/src/main/java/com/mmm/a370333/matchmakermaker/TakeMultResponse.java
@@ -0,0 +1,15 @@
+package com.mmm.a370333.matchmakermaker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by 370333 on 6/16/17.
+ */
+public class TakeMultResponse extends Activity {
+
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.take_multresponse);
+    }
+}
